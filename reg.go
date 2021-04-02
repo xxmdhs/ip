8212e3e8afa2e90944108e1cff43654cce733860
xmdhs <xmdhss@gmail.com>
2021-04-02 12:58:37 +0800

    🐛

--- reg.go
+++ reg.go
@@ -11,7 +11,7 @@ func reg(this js.Value, args []js.Value) interface{} {
 	}
 	reg, err := regexp.Compile(args[0].String())
 	if err != nil {
-		return js.ValueOf(map[string]string{"err": err.Error()})
+		return js.ValueOf(map[string]interface{}{"err": err.Error()})
 	}
 	ls := []string{}
 	if !args[2].Bool() {
